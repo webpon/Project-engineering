3ae1e25b7975077b4204c0dea58bc39273de2790
webpon <2249096563@qq.com>
2021-03-02 03:42:47 +0800

    开发环境图片资源打包配置完成

--- 开发环境/webpack.config.js
+++ 开发环境/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
       // 详细loader配置
       // 不同文件必须配置不同loader处理
       // 处理less css需要安装npm i css - loader style - loader less - loader less - D 
+      //!处理css文件
       {
         // 匹配哪些文件
         test: /\.css$/,
@@ -31,6 +32,7 @@ module.exports = {
           'css-loader'
         ]
       },
+      //!处理less文件
       {
         test: /\.less$/,
         use: [
@@ -40,6 +42,40 @@ module.exports = {
           // 需要下载 less-loader和less
           'less-loader'
         ]
+      },
+     
+      //!处理图片资源
+      {
+        // 问题：默认处理不了html中img图片
+        // 处理图片资源
+        test: /\.(jpg|png|gif)$/,
+        // 使用一个loader
+        // 下载 url-loader file-loader
+        // 当图片体积大于 8192 字节时，默认会使用 file- loader
+        // （虽然代码没有配置 file - loader，但还是需要使用 npm i file - loader - D 安装），
+        // 并且会将配置的选项传递给 file - loader（也就是说上面可以配置 name、outputPath 等选项）
+        loader: 'url-loader',
+        options: {
+          // 图片大小小于8kb，就会被base64处理
+          // 优点: 减少请求数量（减轻服务器压力）
+          // 缺点：图片体积会更大（文件请求速度更慢）
+          limit: 8 * 1024,
+          // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是commonjs
+          // 解析时会出问题：[object Module]
+          // 解决：关闭url-loader的es6模块化，使用commonjs解析
+          esModule: false,
+          // 给图片进行重命名
+          // [hash:10]取图片的hash的前10位
+          // [ext]取文件原来扩展名
+          name: '[hash:10].[ext]'
+        }
+      },
+       //!处理html
+      //将HTML导出为字符串。当编译器需要时，HTML被最小化。
+      {
+        test: /\.html$/,
+        // 处理html文件的img图片（负责引入img，从而能被url-loader进行处理）
+        loader: 'html-loader'
       }
     ]
   },
@@ -49,6 +85,7 @@ module.exports = {
     // html-webpack-plugin
     // 功能：默认会创建一个空的HTML，自动引入打包输出的所有资源（JS/CSS）
     // 需求：需要有结构的HTML文件
+    //打包html模板
     new HtmlWebpackPlugin({
       // 复制 './src/index.html' 文件，并自动引入打包输出的所有资源（JS/CSS）,默认复制到output目录
       template: './src/index.html'
